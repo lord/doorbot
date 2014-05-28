b84974e24664c8b2c0dc7382c1e522354d44e897
David Albert <davidbalbert@gmail.com>
2014-05-28 13:08:05 -0400

    Fix syntax error

--- lib/models.rb
+++ lib/models.rb
@@ -1,5 +1,5 @@
 require 'active_record'
-ActiveRecord::Base.establish_connection(adapter: 'sqlite3' database: 'development.db')
+ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'development.db')
 
 class User < ActiveRecord::Base
 end
