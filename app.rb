7838c5652d33dc5035e19ee61c7befd006867963
David Albert <davidbalbert@gmail.com>
2014-05-28 12:49:41 -0400

    Added some logging messages

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+puts "Doorbot booting..."
+
 require 'active_record'
 require 'json'
 require 'twilio-ruby'
@@ -16,6 +18,8 @@ $unlocker = Unlocker.new
 threads = []
 threads << TwilioWatcher.new($unlocker).thread
 threads << ZulipWatcher.new($unlocker).thread
+
+puts "Doorbot finished booting"
 
 threads.each do |thread|
   thread.join
