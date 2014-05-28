e2a8ff8586e7acea831d45b60b90bd2472b52ce5
David Albert <davidbalbert@gmail.com>
2014-05-28 14:42:12 -0400

    No longer using new_twilio_client

--- app.rb
+++ app.rb
@@ -10,10 +10,6 @@ require './lib/unlocker'
 require './lib/twilio_watcher'
 require './lib/zulip_watcher'
 
-def new_twilio_client
-  return Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
-end
-
 $unlocker = Unlocker.new
 threads = []
 threads << TwilioWatcher.new($unlocker).thread
